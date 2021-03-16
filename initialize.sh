d9030c5154cebb9cae8351506a8f6906ad197b83
Ben Garcia <26657982+BenRGarcia@users.noreply.github.com>
2021-03-16 10:25:31 -0700

    fix

--- initialize.sh
+++ initialize.sh
@@ -3,7 +3,7 @@
 USER_CONTEXT=$(whoami)
 EC2_USER="ec2-user"
 
-if [[ $USER_CONTEXT != $EC2_USER ]]; then
+if [[ $USER_CONTEXT != "$EC2_USER" ]]; then
   echo "Error! This script is only meant to be run on Amazon EC2 as user 'ec2-user'"
   exit 1
 fi
@@ -45,24 +45,6 @@ if ! command -v unzip &>/dev/null; then
   exit 1
 fi
 
-read -p 'What is your full name?     ' NAME
-
-# Make sure name was input
-if [ -z "$NAME" ]; then
-  echo "Error! You didn't type in your full name."
-  echo "Re-run this script and try again."
-  exit 1
-fi
-
-read -p 'What is your email address? ' EMAIL
-
-# Make sure email was input
-if [ -z "$EMAIL" ]; then
-  echo "Error! You didn't type in your email address."
-  echo "Re-run this script and try again."
-  exit 1
-fi
-
 clear_screen() {
   # printf "\033c"
   echo "==================================================================================================="
@@ -70,122 +52,108 @@ clear_screen() {
 
 prompt_to_continue() {
   clear_screen
-  read -p "$1. Press enter to continue."
+  read -rp "$1. Press enter to continue."
 }
 
 prompt_to_continue_done() {
   clear_screen
   echo "$1."
   echo ""
-  read -p "Done. Press enter to continue."
+  read -rp "Done. Press enter to continue."
 }
 
-Q_00="Thank you, $NAME"
+Q_00="Follow the on-screen prompts to configure your Cloud9 EC2 environment"
 prompt_to_continue "$Q_00"
 
 # Update linux package manager
-Q_01="Now we will update yum"
+Q_01="This script will now: update yum"
 prompt_to_continue "$Q_01"
 sudo yum update -y
 prompt_to_continue_done "$Q_01"
 
 # Configure Git
-Q_02="Now we will configure git using your name ($NAME) and email ($EMAIL)"
-prompt_to_continue "$Q_02"
-git config --global user.name "$NAME"
-git config --global user.email "$EMAIL"
-prompt_to_continue_done "$Q_02"
+GIT_USER_NAME=$(git config user.name)
+GIT_USER_EMAIL=$(git config user.email)
+if [ -z "$GIT_USER_NAME" ] || [ -z "$GIT_USER_EMAIL" ]; then
+  Q_02="This script will now: configure git"
+  prompt_to_continue "$Q_02"
+
+  # Prompt for name
+  read -rp 'What is your full name?     ' NAME
+  if [ -z "$NAME" ]; then
+    echo "Error! You didn't type in your full name."
+    echo "Re-run this script and try again."
+    exit 1
+  fi
+
+  # Prompt for email
+  read -rp 'What is your email address? ' EMAIL
+  if [ -z "$EMAIL" ]; then
+    echo "Error! You didn't type in your email address."
+    echo "Re-run this script and try again."
+    exit 1
+  fi
+  git config --global user.name "$NAME"
+  git config --global user.email "$EMAIL"
+  prompt_to_continue_done "$Q_02"
+fi
+
+Q_03="This script will now: update pip and install a git helper"
+prompt_to_continue "$Q_03"
+curl -O https://bootstrap.pypa.io/get-pip.py
+python get-pip.py --user
+rm -rf ./get-pip.py
+python -m pip install git-remote-codecommit
+prompt_to_continue_done "$Q_03"
+
+# Install current version of AWS CDK
+Q_04="This script will now: install and bootstrap the AWS CDK"
+prompt_to_continue "$Q_04"
+npm install -g aws-cdk
+AWS_ACCOUNT_NUMBER=$(aws sts get-caller-identity --query Account --output text)
+if [ -z "$AWS_ACCOUNT_NUMBER" ]; then
+  read -rp 'What is your AWS account number?    ' AWS_ACCOUNT_NUMBER
+fi
+if [ -z "$AWS_ACCOUNT_NUMBER" ]; then
+  echo "Error! You didn't type in your AWS account number."
+  echo "Re-run this script and try again."
+  exit 1
+fi
+export CDK_NEW_BOOTSTRAP=1
+AWS_DEFAULT_REGION=$(aws configure get region)
+if [ -z "$AWS_DEFAULT_REGION" ]; then
+  read -rp 'What is your default AWS region? (us-east-1, us-west-2, etc.)   ' AWS_DEFAULT_REGION
+fi
+if [ -z "$AWS_DEFAULT_REGION" ]; then
+  echo "Error! You didn't type in your default AWS region."
+  echo "Re-run this script and try again."
+  exit 1
+fi
+cdk bootstrap "aws://$AWS_ACCOUNT_NUMBER/$AWS_DEFAULT_REGION"
+prompt_to_continue_done "$Q_04"
 
 # Ensure LTS version of Node.js is installed
-Q_03="Now we will install the current 'LTS' version of Node.js"
+Q_05="This script will now: install the current 'LTS' version of Node.js"
 CURRENT_NODE_LTS_NAME="fermium" # <= will require update to new LTS 10/2021
 EC2_NODE_VERSION=$(node --version)
 CURRENT_NODE_LTS_VERSION=$(nvm version-remote --lts=$CURRENT_NODE_LTS_NAME)
-if [[ $EC2_NODE_VERSION != $CURRENT_NODE_LTS_VERSION ]]; then
-  prompt_to_continue "$Q_03"
+if [[ $EC2_NODE_VERSION != "$CURRENT_NODE_LTS_VERSION" ]]; then
+  prompt_to_continue "$Q_05"
   nvm install --lts=$CURRENT_NODE_LTS_NAME
   nvm alias default lts/$CURRENT_NODE_LTS_NAME
-  nvm uninstall $EC2_NODE_VERSION
-  prompt_to_continue_done "$Q_03"
-fi
-
-# Install current version of TypeScript
-Q_04="Now we will install TypeScript"
-prompt_to_continue "$Q_04"
-npm i -g typescript
-prompt_to_continue_done "$Q_04"
-
-# Uninstall AWS CLI V1 if installed
-Q_05="Now we will uninstall AWS CLI V1"
-AWS_CLI_VERSION="$(echo $(aws --version) | cut -d'/' -f2 | cut -c1-1)"
-echo "Installed version: $AWS_CLI_VERSION"
-if [[ $AWS_CLI_VERSION == "1" ]]; then
-  prompt_to_continue "$Q_05"
-  sudo rm -rf /usr/local/aws
-  sudo rm /usr/local/bin/aws
+  nvm uninstall "$EC2_NODE_VERSION"
   prompt_to_continue_done "$Q_05"
 fi
 
-# Install AWS CLI V2 if not installed
-Q_06="Now we will install AWS CLI V2"
-echo "Installed version: $AWS_CLI_VERSION"
-if [[ $AWS_CLI_VERSION != "2" ]]; then
-  prompt_to_continue "$Q_06"
-  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-  unzip awscliv2.zip
-  sudo ./aws/install
-  rm -rf awscliv2.zip
-  rm -rf ./aws/
-  prompt_to_continue_done "$Q_06"
-fi
+# Install current version of TypeScript
+Q_06="This script will now: install TypeScript"
+prompt_to_continue "$Q_06"
+npm i -g typescript@"~3.9.9" # AWS CDK v1.93.0 TypeScript version
+prompt_to_continue_done "$Q_06"
 
-Q_07="Now we will configure the AWS CLI V2 with your credentials"
+# Increase EBS volume size
+Q_07="This script will now: increase the size of the EBS volume to 30 GiB"
 prompt_to_continue "$Q_07"
-sudo rm -rf ~/.aws/credentials
-sudo rm -rf ~/.aws/config
-aws configure
-AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
-AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
-if [ -z "$AWS_ACCESS_KEY_ID" ]; then
-  echo "Error! You didn't type in your AWS access key id."
-  echo "Re-run this script and try again."
-  exit 1
-fi
-if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
-  echo "Error! You didn't type in your AWS secret access key."
-  echo "Re-run this script and try again."
-  exit 1
-fi
-TECHNICAL_TRAINER_ROLE_ARN="arn:aws:iam::403112560303:role/TechTrainerCloud9Stack-codeCommitReadOnlyAccess982-VWL5HSK3TV97"
-echo "[technical-trainer]" >> ~/.aws/credentials
-echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >>  ~/.aws/credentials
-echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >>  ~/.aws/credentials
-echo "source_profile = default" >> ~/.aws/credentials
-echo "role_arn = $TECHNICAL_TRAINER_ROLE_ARN" >> ~/.aws/credentials
-unset AWS_SECRET_ACCESS_KEY
-prompt_to_continue_done "$Q_07"
-
-# Install current version of AWS CDK
-Q_08="Now we will install the AWS CDK"
-prompt_to_continue "$Q_08"
-npm install -g aws-cdk
-# read -p 'What is your AWS account number?    ' AWS_ACCOUNT_NUMBER
-# if [ -z "$AWS_ACCOUNT_NUMBER" ]; then
-#   echo "Error! You didn't type in your AWS account number."
-#   echo "Re-run this script and try again."
-#   exit 1
-# fi
-# CDK_NEW_BOOTSTRAP=1
-# AWS_DEFAULT_REGION=$(aws configure get region)
-# if [ -z "$AWS_DEFAULT_REGION" ]; then
-#   AWS_DEFAULT_REGION="us-east-1"
-# fi
-# cdk bootstrap "aws://$AWS_ACCOUNT_NUMBER/$AWS_DEFAULT_REGION"
-prompt_to_continue_done "$Q_08"
-
-Q_09="Now we will increase the size of the EBS volume"
-prompt_to_continue "$Q_09"
-# Specify the desired volume size in GiB as a command line argument.
 SIZE=30
 
 # Get the ID of the environment host Amazon EC2 instance.
@@ -193,17 +161,17 @@ INSTANCEID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 
 # Get the ID of the Amazon EBS volume associated with the instance.
 VOLUMEID=$(aws ec2 describe-instances \
-  --instance-id $INSTANCEID \
+  --instance-id "$INSTANCEID" \
   --query "Reservations[0].Instances[0].BlockDeviceMappings[0].Ebs.VolumeId" \
   --output text)
 
 # Resize the EBS volume.
-aws ec2 modify-volume --volume-id $VOLUMEID --size $SIZE
+aws ec2 modify-volume --volume-id "$VOLUMEID" --size "$SIZE"
 
 # Wait for the resize to finish.
 while [ \
   "$(aws ec2 describe-volumes-modifications \
-    --volume-id $VOLUMEID \
+    --volume-id "$VOLUMEID" \
     --filters Name=modification-state,Values="optimizing","completed" \
     --query "length(VolumesModifications)"\
     --output text)" != "1" ]; do
@@ -211,7 +179,7 @@ sleep 1
 done
 
 #Check if we're on an NVMe filesystem
-if [ $(readlink -f /dev/xvda) = "/dev/xvda" ]
+if [ "$(readlink -f /dev/xvda)" = "/dev/xvda" ]
 then
   # Rewrite the partition table so that the partition takes up all the space that it can.
   sudo growpart /dev/xvda 1
@@ -242,23 +210,75 @@ else
     sudo resize2fs /dev/nvme0n1p1
   fi
 fi
-prompt_to_continue_done "$Q_09"
+prompt_to_continue_done "$Q_07"
 
-Q_10="Now we will update pip"
+# Uninstall AWS CLI V1 if installed
+Q_08="This script will now: uninstall AWS CLI V1"
+AWS_CLI_VERSION=$(aws --version | cut -d'/' -f2 | cut -c1-1)
+echo "Installed version: $AWS_CLI_VERSION"
+if [[ $AWS_CLI_VERSION == "1" ]]; then
+  prompt_to_continue "$Q_08"
+  sudo rm -rf /usr/local/aws
+  sudo rm -rf /usr/bin/aws
+  sudo rm /usr/local/bin/aws
+  prompt_to_continue_done "$Q_08"
+fi
+
+# Install AWS CLI V2 if not installed
+Q_09="This script will now: install AWS CLI V2"
+echo "Installed version: $AWS_CLI_VERSION"
+if [[ $AWS_CLI_VERSION != "2" ]]; then
+  prompt_to_continue "$Q_09 "
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  unzip awscliv2.zip
+  sudo ./aws/install
+  rm -rf awscliv2.zip
+  rm -rf ./aws/
+  prompt_to_continue_done "$Q_09"
+fi
+
+Q_10="This script will now: configure the AWS CLI V2 with your credentials"
 prompt_to_continue "$Q_10"
-curl -O https://bootstrap.pypa.io/get-pip.py
-python get-pip.py --user
-rm -rf ./get-pip.py
+sudo rm -rf ~/.aws/credentials
+sudo rm -rf ~/.aws/config
+aws configure
+AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
+AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+  echo "Error! You didn't type in your AWS access key id."
+  echo "Re-run this script and try again."
+  exit 1
+fi
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+  echo "Error! You didn't type in your AWS secret access key."
+  echo "Re-run this script and try again."
+  exit 1
+fi
+TECHNICAL_TRAINER_ROLE_ARN="arn:aws:iam::403112560303:role/TechTrainerCloud9Stack-codeCommitReadOnlyAccess982-VWL5HSK3TV97"
+{
+  echo ""
+  echo "[technical-trainer]"
+  echo "source_profile = default"
+  echo "aws_access_key_id = $AWS_ACCESS_KEY_ID"
+  echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY"
+  echo "role_arn = $TECHNICAL_TRAINER_ROLE_ARN"
+} >> ~/.aws/credentials
+unset AWS_SECRET_ACCESS_KEY
+{
+  echo ""
+  echo "[profile technical-trainer]"
+  echo "region = $AWS_DEFAULT_REGION"
+  echo "output = json"
+} >> ~/.aws/config
 prompt_to_continue_done "$Q_10"
 
-Q_11"Now we will clone all available repositories"
+Q_11="This script will now: clone all available repositories"
 prompt_to_continue "$Q_11"
-python -m pip install git-remote-codecommit
-sts assume-role --role-arn "$TECHNICAL_TRAINER_ROLE_ARN" --role-session-name AWSCLI-Session
+aws sts assume-role --role-arn "$TECHNICAL_TRAINER_ROLE_ARN" --role-session-name AWSCLI-Session
 git clone codecommit://technical-trainer@aai-architecting-on-aws ~/environment/src/aai-architecting-on-aws
 prompt_to_continue_done "$Q_11"
 
 clear_screen
 echo "Your EC2 environment has been configured."
 echo "Close this terminal window and open another before running any additional scripts."
-read -p "Press enter to exit."
+read -rp "Press enter to exit."
