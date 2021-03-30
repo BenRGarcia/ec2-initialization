7237df02a9bede465a0805b8f2758e4cce2c1fe6
Ben Garcia <26657982+BenRGarcia@users.noreply.github.com>
2021-03-29 17:07:10 -0700

    check current volume size before resizing

--- initialize.sh
+++ initialize.sh
@@ -60,47 +60,57 @@ VOLUMEID=$(aws ec2 describe-instances \
     --query "Reservations[0].Instances[0].BlockDeviceMappings[0].Ebs.VolumeId" \
     --output text)
 
-# Resize the EBS volume.
-aws ec2 modify-volume --volume-id "$VOLUMEID" --size "$EBS_VOLUME_SIZE"
+CURRENT_VOLUME_SIZE=$(aws ec2 describe-volumes \
+    --volume-ids "$VOLUMEID" \
+    --query "Volumes[0].Size" \
+    --output text)
 
-# Wait for the resize to finish.
-while [ \
-    "$(aws ec2 describe-volumes-modifications \
-        --volume-id "$VOLUMEID" \
-        --filters Name=modification-state,Values="optimizing","completed" \
-        --query "length(VolumesModifications)" \
-        --output text)" != "1" ]; do
-    sleep 1
-done
+if [[ $EBS_VOLUME_SIZE != "$CURRENT_VOLUME_SIZE" ]]; then
+    echo "Resizing the volume"
+    # Resize the EBS volume.
+    aws ec2 modify-volume --volume-id "$VOLUMEID" --size "$EBS_VOLUME_SIZE"
 
-# Check if we're on an NVMe filesystem
-if [ "$(readlink -f /dev/xvda)" = "/dev/xvda" ]; then
-    # Rewrite the partition table so that the partition takes up all the space that it can.
-    sudo growpart /dev/xvda 1
+    # Wait for the resize to finish.
+    while [ \
+        "$(aws ec2 describe-volumes-modifications \
+            --volume-id "$VOLUMEID" \
+            --filters Name=modification-state,Values="optimizing","completed" \
+            --query "length(VolumesModifications)" \
+            --output text)" != "1" ]; do
+        sleep 1
+    done
 
-    # Expand the size of the file system.
-    # Check if we are on AL2
-    STR=$(cat /etc/os-release)
-    SUB="VERSION_ID=\"2\""
-    if [[ "$STR" == *"$SUB"* ]]; then
-        sudo xfs_growfs -d /
+    # Check if we're on an NVMe filesystem
+    if [ "$(readlink -f /dev/xvda)" = "/dev/xvda" ]; then
+        # Rewrite the partition table so that the partition takes up all the space that it can.
+        sudo growpart /dev/xvda 1
+
+        # Expand the size of the file system.
+        # Check if we are on AL2
+        STR=$(cat /etc/os-release)
+        SUB="VERSION_ID=\"2\""
+        if [[ "$STR" == *"$SUB"* ]]; then
+            sudo xfs_growfs -d /
+        else
+            sudo resize2fs /dev/xvda1
+        fi
+
     else
-        sudo resize2fs /dev/xvda1
-    fi
+        # Rewrite the partition table so that the partition takes up all the space that it can.
+        sudo growpart /dev/nvme0n1 1
 
+        # Expand the size of the file system.
+        # Check if we're on AL2
+        STR=$(cat /etc/os-release)
+        SUB="VERSION_ID=\"2\""
+        if [[ "$STR" == *"$SUB"* ]]; then
+            sudo xfs_growfs -d /
+        else
+            sudo resize2fs /dev/nvme0n1p1
+        fi
+    fi
 else
-    # Rewrite the partition table so that the partition takes up all the space that it can.
-    sudo growpart /dev/nvme0n1 1
-
-    # Expand the size of the file system.
-    # Check if we're on AL2
-    STR=$(cat /etc/os-release)
-    SUB="VERSION_ID=\"2\""
-    if [[ "$STR" == *"$SUB"* ]]; then
-        sudo xfs_growfs -d /
-    else
-        sudo resize2fs /dev/nvme0n1p1
-    fi
+    echo "Skipping volume resize"
 fi
 
 ###############################################################################
